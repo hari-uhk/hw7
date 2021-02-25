b3130a44669ee1004adda12a35e028e928eef612
hari-uhk <hari.uggini@kellogg.northwestern.edu>
2021-02-25 23:20:27 +0530

    final check

--- movies.js
+++ movies.js
@@ -29,7 +29,7 @@ firebase.auth().onAuthStateChanged(async function(user)  {
   
   for (let i=0; i<movies.length; i++) {
     let movie = movies[i]
-    let docRef = await db.collection('watched').doc(`${movie.id}`).get()
+    let docRef = await db.collection('watched').doc(`${movie.id}-${user.uid}`).get()
     let watchedMovie = docRef.data()
     let opacityClass = ''
     if (watchedMovie) {
@@ -37,17 +37,17 @@ firebase.auth().onAuthStateChanged(async function(user)  {
     }
 
     document.querySelector('.movies').insertAdjacentHTML('beforeend', `
-      <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
+      <div class="w-1/5 p-4 movie-${movie.id}-${user.uid} ${opacityClass}">
         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
         <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
       </div>
     `)
 
-    document.querySelector(`.movie-${movie.id}`).addEventListener('click', async function(event) {
+    document.querySelector(`.movie-${movie.id}-${user.uid}`).addEventListener('click', async function(event) {
       event.preventDefault()
-      let movieElement = document.querySelector(`.movie-${movie.id}`)
+      let movieElement = document.querySelector(`.movie-${movie.id}-${user.uid}`)
       movieElement.classList.add('opacity-20')
-      await db.collection('watched').doc(`${movie.id}`).set({})
+      await db.collection('watched').doc(`${movie.id}-${user.uid}`).set({})
     }) 
   } 
   } else {
